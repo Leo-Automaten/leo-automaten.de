6115f466fa2c1d4aae33cd11eb672b8e251f9441
Leo-Automaten <150446639+Leo-Automaten@users.noreply.github.com>
2023-11-11 00:31:45 +0100

    Add files via upload

---
+++ scriptkontakt.js
@@ -0,0 +1,16 @@
+function validateForm() {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    var atLeastOneChecked = false;
+
+    checkboxes.forEach(function(checkbox) {
+        if (checkbox.checked) {
+            atLeastOneChecked = true;
+        }
+    });
+
+    if (atLeastOneChecked) {
+        document.getElementById('myForm').submit();
+    } else {
+        alert('Please select at least one option.');
+    }
+} 
